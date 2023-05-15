89fdaa72820a1b7b67df4567f1e3b7fce405ed58
Yuri <yurisales2000@hotmail.com>
2023-05-15 20:00:16 -0300

    updating header

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 const body = document.querySelector("body");
 const imagemBotaoTrocaTema = document.querySelector(".imagem-botao");
+const header = document.querySelector("header");
 
 botaoAlterarTema.addEventListener("click", () =>{
     const modoeEscuroAtivo = body.classList.contains("modo-escuro");
 
+
     body.classList.toggle("modo-escuro")
+    header.classList.toggle("modo-escuro")
 
     if(modoeEscuroAtivo){
         imagemBotaoTrocaTema.setAttribute("src", "src/imagens/sun.png");
